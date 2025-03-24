8d9b1cf2898fd16c7c4c63734242a4fdcbc1f8cd
Filipe Schenkel <sfschenkel@gmail.com>
2025-03-24 14:25:16 +0100

    Update .list-custom-aliases.py

--- .list-custom-aliases.py
+++ .list-custom-aliases.py
@@ -29,7 +29,7 @@ def render_table(commands_and_usages):
     return table
 
 if __name__ == "__main__":
-"""Parses lines containing # custom-alias (category): usage description"""
+    """Parses lines containing # custom-alias (category): usage description"""
 
     aliases = sys.stdin.read()
 
